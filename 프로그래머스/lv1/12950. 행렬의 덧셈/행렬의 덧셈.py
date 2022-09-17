ceeded617746f32f4516dc99c3bd1b3707ad0d55
junnnn-a <goldfish0907@naver.com>
2022-09-17 23:49:45 +0900

    change file

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,9 +1,9 @@
 def solution(arr1, arr2):
-    hi = []
-    bye = []
+    array_1 = []
+    array_2 = []
     for i in range(len(arr1)) :
         for j in range(len(arr1[i])) :
-            hi.append(arr1[i][j] + arr2[i][j])
-        bye.append(hi)
-        hi = []
-    return bye
+            array_1.append(arr1[i][j] + arr2[i][j])
+        array_2.append(array_1)
+        array_1 = []
+    return array_2
