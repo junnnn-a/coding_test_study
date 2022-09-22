627817959becf2d100a550a7b458e9932250e1a2
junnnn-a <goldfish0907@naver.com>
2022-09-22 23:38:49 +0900

    add markdown

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.py
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.py
@@ -1,8 +1,9 @@
 def solution(num):
+
 #     if num % 2 == 0 :
 #         return 'Even'
-    
 #     else :
 #         return 'Odd'
 
+    ## 한 줄로 간단하게 return
     return 'Even' if num&1 == 0 else 'Odd'
