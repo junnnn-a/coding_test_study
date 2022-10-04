cdd78924d999ea366d662eac55a9b90a0986ec5c
junnnn-a <goldfish0907@naver.com>
2022-10-04 14:39:51 +0900

    change_code

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,8 +1,17 @@
+## 주어진 n 수 이하의 소수의 갯수를 반환하기
+
 def solution(n):
-    num = set(range(2,n+1))
-    
+
+    ## 2부터 n+1 까지를 set으로 만들어서 변수에 저장
+    number = set(range(2,n+1))
+
+    ## i 가 2라면 2를 제외한 2의 배수를 삭제
     for i in range(2,n+1):
-        if i in num:
-            num -= set(range(2*i, n+1, i))
+
+        ## i 가 아까 만들어둔 set number에 있다면! 
+        if i in number:
+            ## 그 해당 배수는 싹 다 삭제를 시킴
+            number -= set(range(2*i, n+1, i))
             
-    return len(num)
+    ## 삭제하고 남은 number 갯수 출력
+    return len(number)
