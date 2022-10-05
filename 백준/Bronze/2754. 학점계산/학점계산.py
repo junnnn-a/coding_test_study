754870d9ebd188df6a7cfabdbdb23c9551c317c9
junnnn-a <97044118+junnnn-a@users.noreply.github.com>
2022-10-05 23:49:29 +0900

    [Bronze V] Title: 학점계산, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,27 @@
+C_score = input()
+if C_score == 'A+':
+    print('4.3')
+elif C_score == 'A0':
+    print('4.0')
+elif C_score == 'A-':
+    print('3.7')
+elif C_score == 'B+':
+    print('3.3')
+elif C_score == 'B0':
+    print('3.0')
+elif C_score == 'B-':
+    print('2.7')
+elif C_score == 'C+':
+    print('2.3')
+elif C_score == 'C0':
+    print('2.0')
+elif C_score == 'C-':
+    print('1.7')
+elif C_score == 'D+':
+    print('1.3')
+elif C_score == 'D0':
+    print('1.0')
+elif C_score == 'D-':
+    print('0.7')
+else:
+    print('0.0')
