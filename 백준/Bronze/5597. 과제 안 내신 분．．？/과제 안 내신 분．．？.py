d065bd81223be456b5b8cb81399275015d109ba8
junnnn-a <goldfish0907@naver.com>
2022-10-06 11:33:30 +0900

    Add Code comments

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,8 +1,11 @@
 ## 교수님이 내준 특별과제를 28명이 제출했는데, 그 중에서 제출 안 한 학생 2명의 출석번호를 구하는 프로그램
 
+## 과제 제출을 한 학생들을 모두 Input으로 받아서 리스트로 저장 -> int 형으로 저장
 submitter = [int(input()) for i in range(28)]
+## 전체 학생을 리스트로 저장 -> int형으로 저장
 all_class = [i+1 for i in range(30)]
 
+## 전체 학생리스트에서 과제 제출 학생 리스트에 없는 학생만 출력
 for i in all_class:
     if i not in submitter:
         print(i)
