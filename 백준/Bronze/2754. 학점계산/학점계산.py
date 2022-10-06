4b18659c9d59df29adccda0ddb6771d8bf6f9a6d
junnnn-a <97044118+junnnn-a@users.noreply.github.com>
2022-10-06 11:19:01 +0900

    [Bronze V] Title: 학점계산, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.py
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -1,27 +1,8 @@
 C_score = input()
-if C_score == 'A+':
-    print('4.3')
-elif C_score == 'A0':
-    print('4.0')
-elif C_score == 'A-':
-    print('3.7')
-elif C_score == 'B+':
-    print('3.3')
-elif C_score == 'B0':
-    print('3.0')
-elif C_score == 'B-':
-    print('2.7')
-elif C_score == 'C+':
-    print('2.3')
-elif C_score == 'C0':
-    print('2.0')
-elif C_score == 'C-':
-    print('1.7')
-elif C_score == 'D+':
-    print('1.3')
-elif C_score == 'D0':
-    print('1.0')
-elif C_score == 'D-':
-    print('0.7')
-else:
-    print('0.0')
+score = {'A+': 4.3, 'A0': 4.0, 'A-': 3.7,
+        'B+': 3.3, 'B0': 3.0, 'B-': 2.7,
+        'C+': 2.3, 'C0': 2.0, 'C-': 1.7,
+        'D+': 1.3, 'D0': 1.0, 'D-': 0.7,
+        'F' : 0.0}
+
+print(score[C_score])
