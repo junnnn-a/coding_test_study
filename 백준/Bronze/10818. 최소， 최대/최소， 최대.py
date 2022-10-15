e2d5a861e5749c0a0c96c4edf4dcb8f24a1627b5
junnnn-a <goldfish0907@naver.com>
2022-10-15 23:56:04 +0900

    Add Code Commits

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -2,7 +2,9 @@
 
 N = int(input())
 
-# numbers = [int(input()) for i in range(N)]
+# numbers = [int(input()) for i in range(N)]    ## 이건 우리가 input 을 한 줄씩 받을 때 사용!
+
+## 이건 우리가 input() 을 한 줄이 아니라 옆으로 띄어쓰기로 받을 때 (ex. 20 10 35 30 7)
 numbers = list(map(int,  input().split()))
 
 print(min(numbers), max(numbers))
