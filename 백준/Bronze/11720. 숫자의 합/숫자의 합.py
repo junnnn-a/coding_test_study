be451b4761d8fdb7b095250fa4e3239fedd206ea
junnnn-a <goldfish0907@naver.com>
2022-10-18 23:47:14 +0900

    Add Code Commits

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.py
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.py
@@ -5,9 +5,12 @@ numbers = input()
 result = []
 answer = 0
 
+## N 개의 숫자가 공백으로 쓰여져있으니까 N 만큼 반복
 for i in range(N):
+    ## 문자열로 받은 input이 공백없이 들어오니까 0번째부터 i+1까지 잘라서 리스트에 저장
     result.append(numbers[i:i+1])
-
+    
+    ## 리스트에 각 요소를 저장한 것을 바탕으로 answer이라는 변수에 정수 형태로 더해주기
     answer += int(numbers[i])
 
 print(answer)
