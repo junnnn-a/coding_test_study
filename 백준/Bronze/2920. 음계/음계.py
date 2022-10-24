40228fa20e0d5d073814e8d5ec6e5041ced5c0c9
junnnn-a <97044118+junnnn-a@users.noreply.github.com>
2022-10-24 11:50:24 +0900

    [Bronze II] Title: 음계, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,13 @@
+## 연주한 순서가 주어졌을 때, 이것이 ascending인지, descending인지, 아니면 mixed인지 판별하는 프로그램
+
+scale = list(map(int, input().split()))
+
+
+if scale == [1, 2, 3, 4, 5, 6, 7, 8] :
+    print('ascending')
+
+elif scale == [8, 7, 6, 5, 4, 3, 2, 1]:
+    print('descending')
+    
+else:
+    print('mixed')
