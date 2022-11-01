33bdc595bd55af4c574a56fcc53bd0a1f958471e
junnnn-a <goldfish0907@naver.com>
2022-11-01 21:08:49 +0900

    Add Code Commits

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
@@ -6,5 +6,6 @@ N = int(input())
 for i in range(N):
     ## i 가 한 번 반복할 때 j가 실행 (이중 for문)
     for j in range(i+1):
+        ## 이중 반복문이 돌면서 첫 째 줄에는 별 1개, 두 번째 줄에는 2개로 증가하는 형태!
         print('*', end='')
     print()
