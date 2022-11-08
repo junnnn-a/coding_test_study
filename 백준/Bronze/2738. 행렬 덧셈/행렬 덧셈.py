e3114fa802131f67e5fc83d49b29d7e0c29b02ea
junnnn-a <goldfish0907@naver.com>
2022-11-08 23:31:24 +0900

    Add Code Commits

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,18 +1,24 @@
 ## N*M크기의 두 행렬 A와 B가 주어졌을 때, 두 행렬을 더하는 프로그램
+## N*M 크기니까 input으로 행렬 크기 받기
 N, M = map(int, input().split())
 
+## N*M 행렬을 각각 따로 만들기
 A = []
 B = []
 
+## A 행렬 만들기
 for i in range(N):
     A.append(list(map(int, input().split())))
 
+## B 행렬 만들기
 for i in range(N):
     B.append(list(map(int, input().split())))
 
+## 만들어진 A 행렬에다가 B 행렬 더하기
 for i in range(N):
     for j in range(M):
         A[i][j] += B[i][j]
 
+        ## 더해진 A 행렬 요소별로 묶어서 출력
         print(A[i][j], end=' ')
     print()
