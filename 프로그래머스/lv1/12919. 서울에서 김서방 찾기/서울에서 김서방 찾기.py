8423926085e0e76dfe573713495d7f416f0c8d0d
junnnn-a <goldfish0907@naver.com>
2023-02-12 19:48:13 +0900

    Add Code Commits and Code

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,6 +1,13 @@
+## String형 배열 seoul의 element중 "Kim"의 위치 x를 찾아, "김서방은 x에 있다"는 String을 반환하는 함수
+
 def solution(seoul):
     answer = 0
+    ## seoul 리스트의 길이만큼 반복 -> 나중에 인덱스 값을 저장하기 위해서 리스트 길이만큼 반복~
     for i in range(len(seoul)):
+
+        ## 만약에 리스트에 'Kim'이라는 문자열이 있으면 그 해당 인덱스를 answer에 저장
         if seoul[i] == 'Kim':
             answer += i
+    
+    ## 저장 된 index 수를 가지고 원하는 답 출력
     return "김서방은 " + str(answer) + "에 있다"
--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
@@ -4,6 +4,7 @@ def solution(n):
     answer = 0
     ## 1부터 정수 n까지 전체 수를 반복
     for i in range(1, n+1):
+        
         ## 만약에 n을 i로 나눴을 때 나머지가 0이면 약수이기 때문에 answer에 더해서 약수의 합 구하기
         if n % i == 0:
             answer += i
