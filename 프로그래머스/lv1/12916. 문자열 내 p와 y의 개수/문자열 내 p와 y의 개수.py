b1fa68f13d12e0d37e3859f9ebcc08295abda03c
junnnn-a <goldfish0907@naver.com>
2023-02-13 23:18:51 +0900

    Add Code Commits and Code

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
@@ -1,9 +1,14 @@
 def solution(s):
     answer = True
+    
+    ## 문자열 s 전체를 소문자로 변경해서 s_lower 이라는 변수에 저장
     s_lower = s.lower()
     
+    ## 만약에 s_lower에 있는 'p'의 갯수와 'y'의 갯수가 같으면 answer 는 True
     if s_lower.count('p') == s_lower.count('y'):
         answer = True
+    
+    ## 갯수가 같지 않다면 answer 는 False
     else:
         answer = False
 
