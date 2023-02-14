f0a4f5eff55db5f47c6f0b78458af70c1e5bfb68
junnnn-a <goldfish0907@naver.com>
2023-02-14 22:57:34 +0900

    Add Code Commits and Code

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -1,9 +1,16 @@
 def solution(a, b):
     answer = 0
+    
+    ## a가 b보다 큰 경우! 
     if a < b:
+        ## a부터 b+1까지로 설정을 해야지 b까지 포함이 됨! -> 그리고 값을 answer에 더하기
         for i in range(a, b+1):
             answer += i
+    
+    ## b가 a보다 큰 경우!
     else:
+        ## b부터 a+1까지 하나씩 answer에 더하기!
         for i in range(b, a+1):
             answer += i
+            
     return answer
