f99f948baf017712f46a5f61c70a1dba30f21174
junnnn-a <goldfish0907@naver.com>
2023-02-15 23:31:34 +0900

    Add Code Commits and Code

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,15 +1,19 @@
+## 문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수
+
 def solution(s):
     answer = True
-    if len(s) == 4:
+    
+    ## 만약에 문자열 s의 길이가 4또는 6일 때만 해당 함수 실행
+    if len(s) == 4 or len(s) == 6:
+        
+        ## 문자열 s에 int()를 씌워서 오류가 발생하면 False로 예외 처리
         try:
             s = int(s)
         except:
             answer = False
-    elif len(s) == 6:
-        try:
-            s = int(s)
-        except:
-            answer = False
+    
+    ## 문자열 s 길이가 4또는 6이 아닌 모든 경우는 False
     else:
         answer = False
+        
     return answer
