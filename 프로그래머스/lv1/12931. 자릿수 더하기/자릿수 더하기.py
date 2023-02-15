73ab641036e1b517e679f7fb3b7e8721d6b94b3f
junnnn-a <97044118+junnnn-a@users.noreply.github.com>
2023-02-16 00:24:27 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,7 +1,11 @@
 def solution(n):
     answer = 0
+    # print(type(n))
     n = str(n)
-    
     for i in range(len(n)):
         answer += int(n[i])
+    
+    # a = 'answer'
+    # print(a[1])
+
     return answer
