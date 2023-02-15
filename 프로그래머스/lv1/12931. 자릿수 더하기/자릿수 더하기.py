d0f4fe5cb3b3a581643b1d630dd105d4e4a26db1
junnnn-a <goldfish0907@naver.com>
2023-02-16 00:27:40 +0900

    Add Code Commits and Code

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,11 +1,14 @@
+## 자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수
+
 def solution(n):
     answer = 0
     # print(type(n))
+    
+    ## n이 자연수기 때문에 자릿수를 하나씩 더할려면 str() 형으로 바꿔줌
     n = str(n)
+    
+    ## n은 이제 문자열이니까 자릿수를 기준으로 수를 하나씩 뜯어서 answer에 더해주기(더해줄 때는 int()형으로 변경)
     for i in range(len(n)):
         answer += int(n[i])
-    
-    # a = 'answer'
-    # print(a[1])
 
     return answer
