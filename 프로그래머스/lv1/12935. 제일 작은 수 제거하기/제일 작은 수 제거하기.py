fdd17a068bc08cda27a066731229bd77acf24709
junnnn-a <goldfish0907@naver.com>
2023-02-28 18:35:39 +0900

    Add Code Commits and Code

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -1,12 +1,13 @@
+## 정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수
+
 def solution(arr):
     answer = []
     
-    if len(arr) == 1:
+    if len(arr)==1:
         answer.append(-1)
-        
     else:
-        arr.remove(min(arr))
-        for i in arr:
-            answer.append(i)
-                
+        min_num = min(arr)
+        # answer.append(1)
+        arr.remove(min_num)
+        answer = arr
     return answer
