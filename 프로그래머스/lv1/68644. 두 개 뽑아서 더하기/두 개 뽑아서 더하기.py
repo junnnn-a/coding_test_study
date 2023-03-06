a087a9c0f114a693ff04c95b7c7e011532cf9271
junnnn-a <goldfish0907@naver.com>
2023-03-06 21:21:24 +0900

    Add Code Commits and Code

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,12 +1,8 @@
 def solution(numbers):
     answer = []
     for i in range(len(numbers)-1):
-        # print(numbers[i])
         for j in range(i+1, len(numbers)):
-            # print('i', numbers[i])
-            # print('j', numbers[j])
             answer.append(numbers[i]+numbers[j])
-            # print(answer)
     answer = list(set(answer))
     answer.sort()
     return answer
