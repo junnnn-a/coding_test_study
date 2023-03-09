23f6363254d0cca8a38e5286258d0fc6a86d00be
junnnn-a <goldfish0907@naver.com>
2023-03-09 20:04:34 +0900

    Add Code Commits and Code

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -3,10 +3,18 @@
 
 def solution(numbers):
     answer = []
-    for i in range(len(numbers)-1):
-        for j in range(i+1, len(numbers)):
-            answer.append(numbers[i]+numbers[j])
-    answer = list(set(answer))
-    answer.sort()
     
+    ## 2개의 숫자를 뽑아서 다 더해서 return
+    ## 첫 번째 숫자 i 는 index 0부터 마지막 전까지 숫자를 반복
+    for i in range(len(numbers)-1):
+        ## 두 번째 숫자 j의 index는 i를 기준으로 항상 1보다 뒤에 있고 마지막까지 숫자를 반복
+        for j in range(i+1, len(numbers)):
+            ## i 와 j index에 해당하는 숫자를 각 더해서 answer라는 list 에 추가
+            answer.append(numbers[i]+numbers[j])
+    
+    ## list에 중복 된 숫자 제거를 위해서 set()함수를 사용
+    answer = list(set(answer))
+    
+    ## 정렬 후 return
+    answer.sort()
     return answer
