9d798c0ca4044cb31ff51c41e35750293584dd3b
junnnn-a <goldfish0907@naver.com>
2023-03-15 22:45:11 +0900

    Add Code Commits and Code

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,9 +1,16 @@
+## 배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수
+
 def solution(arr):
     answer = []
-
+    
+    ## 배열의 길이만큼 반복
     for i in range(len(arr)):
+        
+        ## 만약에 배열의 첫 번째 인덱스는 answer 리스트에 값이 없을 거니까 무조건 값 추가
         if i==0:
             answer.append(arr[i])
+        
+        ## 만약 arr의 i번째 요소가 answer에 들어있는 마지막 요소와 같지 않다면 answer 리스트에 값 추가
         if arr[i] != answer[-1]:
             answer.append(arr[i])
     
