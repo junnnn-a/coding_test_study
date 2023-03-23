ece8fbfddc331ce0ed07f3e417b97a46100bac71
junnnn-a <goldfish0907@naver.com>
2023-03-23 21:43:47 +0900

    Add Code Commits and Code

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -5,6 +5,7 @@ def solution(numbers):
     
     ## 0에서 9까지 숫자를 반복
     for i in range(10):
+        
         ## 만약에 i 숫자가 numbers라는 배열에 없으면 answer에 없는 숫자 더하기
         if i not in numbers:
             answer += i
