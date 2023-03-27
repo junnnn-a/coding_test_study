0da43d6d45cfb5bc15a4c0ecb5231a0f1846c51e
junnnn-a <goldfish0907@naver.com>
2023-03-27 17:29:00 +0900

    Add Code Commits and Code

--- 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
+++ 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
@@ -1,8 +1,9 @@
+## 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴하는 함수
+
 def solution(x, n):
     answer = []
     
-    # for i in range(2, 11, 2):
-
+    ## 만약 시작하는 x가 양수일 때
     if x > 0:
         for i in range(x, x*n+1, x):
             answer.append(i)
