6a6335cc6ebe2286d187210847cd3dd75e39e563
junnnn-a <goldfish0907@naver.com>
2023-03-28 16:21:41 +0900

    Add Code Commits and Code

--- 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
+++ 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
@@ -5,12 +5,19 @@ def solution(x, n):
     
     ## 만약 시작하는 x가 양수일 때
     if x > 0:
+        ## x부터 x와 n을 곱한 수까지 반복이니까 +1 을 해줌! (간격은 x만큼)
         for i in range(x, x*n+1, x):
             answer.append(i)
+    
+    ## 시작하는 x가 음수일 때
     elif x < 0:
+        ## x부터 x와 n을 곱한 수까지 반복이어서 -1을 해줌 (간격은 동일하게 x만큼)
         for i in range(x, x*n-1, x):
             answer.append(i)
+    
+    ## 저 위에 두 개 조건문만 주면 실행결과가 실패가 됨.. x가 0일 때도 고려해 줘야함!
     else:
+        ## n만큼 반복해서 리스트에 0을 추가
         for i in range(n):
             answer.append(0)
         
