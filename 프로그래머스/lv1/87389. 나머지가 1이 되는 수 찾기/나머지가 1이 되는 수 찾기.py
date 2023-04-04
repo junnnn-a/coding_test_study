b30a04e77e8e92aa762a333460a2cb676994dd39
junnnn-a <goldfish0907@naver.com>
2023-04-04 11:57:19 +0900

    Add Code Commits and Code

--- 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
@@ -1,8 +1,13 @@
+## n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return 하도록 solution 함수
+
 def solution(n):
     answer = 0
     
-    for i in range(1, n):
-        if n%i == 1:
-            answer += i
+    ## n보다 작은 수를 순차적으로 반복 (x는 자연수기 때문에 1부터 시작)
+    for x in range(1, n):
+        ## 나머지가 1인 가장 작은 수니까 순차적으로 수가 들어와서 나머지가 1이 되는 수를 찾으면 break
+        if n%x == 1:
+            answer += x
             break
+            
     return answer
