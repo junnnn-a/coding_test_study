190b085788287943c26c5ea19ebacb4d0a682941
junnnn-a <goldfish0907@naver.com>
2023-04-06 20:42:53 +0900

    Add Code & Add Comments

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++
@@ -1,9 +0,0 @@
-def solution(arr1, arr2):
-    array_1 = []
-    array_2 = []
-    for i in range(len(arr1)) :
-        for j in range(len(arr1[i])) :
-            array_1.append(arr1[i][j] + arr2[i][j])
-        array_2.append(array_1)
-        array_1 = []
-    return array_2
