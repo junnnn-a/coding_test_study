c7d89d2ba3ff38957cffedf8bc7b7f135a12682e
junnnn-a <goldfish0907@naver.com>
2023-04-11 18:15:46 +0900

    Add Code & Add Comments

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,10 @@
+
+
 def solution(participant, completion):
     participant.sort()
     completion.sort()
     
-    for p, c in zip(participant, completion):
-        if p != c:
-            return(p)
+    for p, c in zip (participant, completion):
+        if p != c :
+            return p
     return participant[-1]
