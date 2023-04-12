1589dd1d08e5ddb5740a67414a26cd49110ad1c9
junnnn-a <goldfish0907@naver.com>
2023-04-12 21:09:50 +0900

    Add Code & Add Comments

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,15 +1,9 @@
 def solution(array, commands):
     answer = []
-    
-    # print(array[0:3])
-    
-    for j in range(len(commands)):
-        for i in range(len(array)):
-            array_crop = array[commands[j][0]-1 : commands[j][1]]
-        array_crop.sort()
-        # print(array_crop)
-        # print(array_crop[0])
-        
-        answer.append(array_crop[commands[j][2]-1])
-    
+    for i in range(len(commands)):
+        a = array[commands[i][0]-1 : commands[i][1]]
+        a.sort()
+        # print(a)
+        # print(commands[i][2]-1)
+        answer.append(a[commands[i][2]-1])
     return answer
