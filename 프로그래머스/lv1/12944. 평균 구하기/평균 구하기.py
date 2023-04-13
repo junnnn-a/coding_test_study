3e9b1335fc229a8c24261dc6ae2a58ff88eea7b6
junnnn-a <goldfish0907@naver.com>
2023-04-13 22:00:31 +0900

    Add Code & Add Comments

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.py
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.py
@@ -1,3 +1,5 @@
+## 정수를 담고 있는 배열 arr의 평균값을 return하는 함수
+
 def solution(arr):
     ## 빈 변수 한 개 생성
     answer = 0
@@ -7,5 +9,6 @@ def solution(arr):
         ## 요소를 아까 빈 변수 answer에 저장
         answer += i
     
-    ## 평균(자료값의 총합 / 자료의 개수)을 구해야 하니까 arr 요소를 다 더한거를 arr 배열의 길이로 나눠주기!
+    ## 평균을 구해야 하니까 arr 요소를 다 더한거를 arr 배열의 길이로 나눠주기!
+    ## 평균 (자료값의 총합 / 자료의 개수)
     return (answer / len(arr))
