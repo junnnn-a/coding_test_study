cf858366206b7c3e6916808eff8c913bc7532bee
junnnn-a <goldfish0907@naver.com>
2023-04-21 10:21:54 +0900

    Add Comments

--- 프로그래머스/lv1/70128. 내적/내적.py
+++ 프로그래머스/lv1/70128. 내적/내적.py
@@ -1,5 +1,11 @@
+## a와 b의 내적을 return 하도록 solution 함수
+
 def solution(a, b):
     answer = 0
+    
+    ## a, b의 길이가 같으니까 배열의 길이만큼 반복
     for i in range(len(a)):
+        ## 두 배열을 내적한 후 반환 (자릿수끼리 곱해서 answer에 더해주기)
         answer += a[i] * b[i]
+        
     return answer
