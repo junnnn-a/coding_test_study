69de324612b8b812b3317972198d1618a131562b
junnnn-a <goldfish0907@naver.com>
2023-04-25 21:57:25 +0900

    Add Comments

--- 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
@@ -7,7 +7,7 @@ def solution(n):
     for x in range(1, n):
         
         ## 나머지가 1인 가장 작은 수니까 순차적으로 수가 들어와서 나머지가 1이 되는 수를 찾으면 break
-        if n%x == 1:
+        if n % x == 1:
             answer += x
             break
             
