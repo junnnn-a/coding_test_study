0ef77992e837a5cfa337d1fb025687b1bb194679
junnnn-a <goldfish0907@naver.com>
2023-05-03 17:00:32 +0900

    Add Comments

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,14 +1,26 @@
+## left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수
+
 def solution(left, right):
     answer = 0
     divisor = []
+    
+    ## left부터 right까지의 모든 수를 반복
     for i in range(left, right+1):
+        ## 숫자 1개의 약수의 개수를 구해야 함
         for j in range(1, i+1):
+            
+            ## 만약 약수라면 divisor라는 리스트에 추가
             if i % j == 0 :
                 divisor.append(j)
-            # print(divisor)
+        
+        ## 약수의 개수가 짝수라면 최종 수(answer)에 수를 더해주기
         if len(divisor) %2 == 0:
             answer += j
+        ## 약수의 개수가 홀수라면 최종 수(answer)에 수를 빼주기
         else:
             answer -= j
+        
+        ## 한 수의 약수의 갯수를 구하는게 끝나면 리스트 초기화
         divisor = []
+        
     return answer
