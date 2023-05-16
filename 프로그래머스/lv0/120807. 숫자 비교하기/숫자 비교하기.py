edfab348de6e4a48ba5006baafce101f6058a3f6
junnnn-a <goldfish0907@naver.com>
2023-05-16 15:49:15 +0900

    Add Comments

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.py
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.py
@@ -1,7 +1,11 @@
+## 두 수가 같으면 1 다르면 -1을 retrun하는 solution 함수
+
 def solution(num1, num2):
-    answer = 0
+    
+    ## 만약 두 수가 같으면 1을 return
     if num1 == num2:
-        answer += 1
+        return 1    
+    
+    ## 두 수가 다르면 -1을 return
     else:
-        answer += -1
-    return answer
+        return -1
