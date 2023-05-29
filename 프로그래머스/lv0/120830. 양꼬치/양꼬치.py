da2db30fc1d7b309ce33e284c888f09098c2425a
junnnn-a <goldfish0907@naver.com>
2023-05-29 09:41:16 +0900

    Add Comments

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.py
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.py
@@ -1,4 +1,5 @@
+## 양꼬치 n인분과 음료수 k개를 먹었다면 총얼마를 지불해야 하는지 return 하도록 solution 함수
+
 def solution(n, k):
     answer = (n * 12000) + (k*2000) - (n//10*2000)
-    
     return answer
--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.py
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -1,3 +1,5 @@
+## 좌표 dot이 사분면 중 어디에 속하는지 1, 2, 3, 4 중 하나를 return 하도록 solution 함수
+
 def solution(dot):
     
     if dot[0]>0 and dot[1]>0:
